dc66e95aa13c1c27610ccc0357e1f6fa01bb278e
Katie Moosman <moosman.katie@gmail.com>
2019-01-09 05:52:13 +0000

    Automatically backed up by Learn

--- lib/musicLibraryController.rb
+++ lib/musicLibraryController.rb
@@ -117,7 +117,7 @@ class MusicLibraryController
     song_name = songs_by_name[input]
     if song_selected > 0
       if songs_by_name[input] != nil
-        puts "Playing #{songs_by_name[input].name} by #{songs_by_name[input].artist.name}" 
+        puts "Playing #{songs_by_name[input].name} by #{songs_by_name[input].artist.name}" unless songs_by_name[input] == nil
       end
     end
 
